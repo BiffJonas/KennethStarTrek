0bae113610962bd8ac34e8c061e590a5367d142d
Johannes Johansson <johannesjoh00@gmail.com>
2023-10-23 15:36:34 +0200

    submitted

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-app.use(express.static('./public'))
- 
-app.listen(5000,()=>{
-    console.log("server listne")
-    
-})
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 5000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
