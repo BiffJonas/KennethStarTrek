22949f5a9b1da769defbf2cd4dd0e47c2de9c329
alfredsandare <alfred.s.sandare04@gmail.com>
2023-10-26 13:12:56 +0200

    lägga till items

--- public/logic.js
+++ public/logic.js
@@ -1,5 +1,20 @@
-const bodyEl = document.querySelector('body')
+const bodyEl = document.querySelector('body');
+const errorText = 'Error: Input must include "Kenneth"';
 
 function colorYellow () {
-    bodyEl.style.backgroundColor = 'yellow'
+    bodyEl.style.backgroundColor = 'yellow';
+}
+
+function addTitle() {
+    var ul = document.getElementById("episode_titles");
+    var li = document.createElement("li");
+    var text = document.getElementById("add_title").value;
+    if (text.includes("Kenneth")) {
+        li.appendChild(document.createTextNode(text));
+        ul.appendChild(li);
+        document.getElementById("error_text").textContent = "";
+    }
+    else {
+        document.getElementById("error_text").textContent = errorText;
+    }
 }
