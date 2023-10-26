babc9a0f9986c3970bedb07b1d03e84e0c20483f
Johannes Johansson <johannesjoh00@gmail.com>
2023-10-26 13:50:18 +0200

    js stuff idk

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const serviceWorker = require('./service-worker.js')
+
 app.use(express.static('./public'))
 
 // app.get('/', (req, res) =>{
@@ -11,6 +13,10 @@ app.use(express.static('./public'))
 // app.get('/about', (req, res) =>{
 
 // })
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js');
+}
 
 app.all('*', (req,res) =>{
     res.status(404).send('page not found')
