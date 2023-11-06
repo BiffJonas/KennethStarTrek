1a30309eea0ac2a71940e1550b0a7a775ba43f29
Johannes Johansson <johannesjoh00@gmail.com>
2023-11-06 14:32:51 +0100

    dawd

--- public/logic.js
+++ public/logic.js
@@ -1,60 +1,54 @@
-const bodyEl = document.querySelector('body');
+const bodyEl = document.querySelector("body");
 const errorText = 'Error: Input must include "Kenneth"';
 
-function colorYellow () {
-    bodyEl.style.backgroundColor = 'yellow';
+function colorYellow() {
+	bodyEl.style.backgroundColor = "yellow";
 }
 
 function addTitle() {
-    var ul = document.getElementById("episode_titles");
-    var li = document.createElement("li");
-    var text = document.getElementById("add_title").value;
-    if (text.includes("Kenneth")) {
-        li.appendChild(document.createTextNode(text));
-        ul.appendChild(li);
-        document.getElementById("error_text").textContent = "";
-    }
-    else {
-        document.getElementById("error_text").textContent = errorText;
-    }
+	var ul = document.getElementById("episode_titles");
+	var li = document.createElement("li");
+	var text = document.getElementById("add_title").value;
+	if (text.includes("Kenneth")) {
+		li.appendChild(document.createTextNode(text));
+		ul.appendChild(li);
+		document.getElementById("error_text").textContent = "";
+	} else {
+		document.getElementById("error_text").textContent = errorText;
+	}
 }
 
-
 //To enhance user experience while offline
-
 
 let registration = null;
 
 function register_service__worker() {
-    if('serviceWorker' in navigator ) {
-        window.navigator.serviceWorker.register('./service-worker.js', {scope: './'})
-            .then(res =>{
-                registration = res;
-                console.log('Service worker successfully registerd')
-                
-            })
-            .catch(err => {
-                console.log('Service worker not registerd')
-
-            })
-    }
+	if ("serviceWorker" in navigator) {
+		window.navigator.serviceWorker
+			.register("./service-worker.js", { scope: "./" })
+			.then((res) => {
+				registration = res;
+				console.log("Service worker successfully registerd");
+			})
+			.catch((err) => {
+				console.log("Service worker not registerd");
+			});
+	}
 }
 
 function unregister_service__worker() {
-    navigator.serviceWorker.getRegistrations()
-        .then(registrations => {
-            registrations.forEach(registration => {
-                registration.unregister()
-                console.log('Service worker unregistered')
-                
-            })
-        })
-        .catch(err=> {
-            console.log('could not register service worker')
-            
-        })
+	navigator.serviceWorker
+		.getRegistrations()
+		.then((registrations) => {
+			registrations.forEach((registration) => {
+				registration.unregister();
+				console.log("Service worker unregistered");
+			});
+		})
+		.catch((err) => {
+			console.log("could not register service worker");
+		});
 }
 
-
-register_service__worker()
-unregister_service__worker()
+register_service__worker();
+unregister_service__worker();
